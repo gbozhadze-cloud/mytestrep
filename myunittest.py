de62a02a0dd713aca4a3cb73b53bb2cea8a46815
Giga <gbozhadze@gmail.com>
2025-12-22 17:19:56 +0400

    commit

--- myunittest.py
+++ myunittest.py
@@ -3,20 +3,49 @@
 # შექმენით Calculator კლასი add, subtract, multiply, divide მეთოდებით. დაწერეთ unittest რომელიც ამოწმებს ყველა მეთოდს.
 # გაითვალისწინეთ 0-ზე გაყოფაც.
 # გამოიყენეთ unittest მოდული
+# def add (a,b):
+#     return a + b
 #
+# def subtract (a,b):
+#     return a - b
 #
+# def multiply (a,b):
+#     return a * b
 #
+# def divide (a,b):
+#     return a / b
 #
-# #2 unittest2
+# assert add(1,2) == 3
+# assert subtract(4,3) == 1
+# assert multiply(1,2) == 2
+# assert divide(9,3) == 3
+#
+# print("ყველა ტესტი წარმატებულია")
 
+# #2 unittest2
 # შექმენით ფუნქციები deposit და withdraw მეთოდებით. დაწერეთ unittest რომელიც ამოწმებს:
-#
 # - სწორი ბალანსი
-#
 # - უარყოფითი თანხის შეტანისას შეცდომა
-#
 # - თანხის გამოტანა ბალანსზე მეტისას შეცდომა
-#
+import unittest
+
+mybalance = 0
+
+def deposit(x):
+    if x <= 0:
+        print("შესატანი თანხა უნდა იყოს 0 ზე მეტი")
+    return mybalance + x
+
+class mydeposittest(unittest.TestCase):
+    def deposit(self):
+        return deposit(10)
+
+
+
+
+
+
+
 #
 # #3 unittest3
 #
